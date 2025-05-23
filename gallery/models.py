bf6da6ff44a56ca9ba237d46328e6817ff6c8388
VikaChubykina <vtubukina90@gmail.com>
2025-05-23 14:46:47 +0300

    Add Image model

--- gallery/models.py
+++ gallery/models.py
@@ -5,3 +5,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category)
+    created_date = models.DateField(auto_now_add=True)
+    age_limit = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
